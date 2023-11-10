60ce17acb35fd1074b4fdc2b66894efe352830c9
AlltidSemester1337 <wowspelarn@gmail.com>
2023-11-10 15:15:00 +0100

    Implement integrations endpoints

--- router/integrations.py
+++ router/integrations.py
@@ -20,20 +20,23 @@ def get_db():
 
 db_dep = Annotated[Session, Depends(get_db)]
 
-#TODO FIX
+
 @router.get("/by_restaurant")
 def get_all_integrations_by_restaurant(db: db_dep, restaurant_name: str = Query(min_length=2, max_length=100)):
-    return db.query(models.Integrations).filter(models.Integrations.restaurant.casefold() == restaurant_name.casefold())
+    return db.query(models.Integrations).filter(
+        models.Integrations.restaurant == restaurant_name).all()
+
 
 @router.get("/")
 def get_all_integrations(db: db_dep):
     return db.query(models.Integrations).all()
 
 
-#TODO FIX
+# TODO FIX
 @router.delete("/{id}", status_code=204)
 def delete_integration(db: db_dep, id: int = Path(gt=-1)):
     db.query(models.Integrations).filter(models.Integrations.id == id).delete()
+    db.commit()
 
 
 @router.post("/", status_code=201)
